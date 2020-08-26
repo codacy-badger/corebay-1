dd6f70de78017dbe57418a54c1e4bf4361352a10
Mehavoid <yeer2nado3ooqp1ue@protonmail.com>
2020-08-26 08:59:48 +0300

    Split auth test into two parts
    PR-URL: https://github.com/Mehavoid/corebay/pull/24

---
+++ test/unit.authfails.js
@@ -0,0 +1,71 @@
+const assert = require('assert').strict;
+
+const config = require('../test/files/config');
+const Client = require('../lib/client');
+
+assert(config);
+assert(Client);
+
+(async () => {
+
+  const emptyUsernamePromise = assert.rejects(
+    async () => (await new Client()),
+    error => {
+      assert.strictEqual(error.name, 'AuthorizationError');
+      assert.strictEqual(error.message, 'You must enter a username');
+      return true;
+    }
+  );
+
+  const emptyPasswordPromise = assert.rejects(
+    async () => (await new Client(config.username)),
+    error => {
+      assert.strictEqual(error.name, 'AuthorizationError');
+      assert.strictEqual(error.message, 'Your password field was not complete');
+      return true;
+    }
+  );
+
+  const incorrectUserMessage = (
+    'Sorry, we could not find ' +
+    'a member using those log in details.'
+  );
+
+  const incorrectUserPromise = assert.rejects(
+    async () => (await new Client('testtesttest', '1234567')),
+    error => {
+      assert.strictEqual(error.name, 'AuthorizationError');
+      assert.strictEqual(error.message, incorrectUserMessage);
+      return true;
+    }
+  );
+
+
+  const incorrectPasswordPromise = assert.rejects(
+    async () => (await new Client(config.username, '1234567')),
+    error => {
+      assert.strictEqual(error.name, 'AuthorizationError');
+      assert.strictEqual(error.message, 'Username or password incorrect');
+      return true;
+    }
+  );
+
+
+  const promises = [
+    emptyUsernamePromise,
+    emptyPasswordPromise,
+    incorrectUserPromise,
+    incorrectPasswordPromise,
+  ];
+
+  try {
+
+    await Promise.all(promises);
+
+  } catch (e) {
+
+    console.error(e.stack);
+    process.exit(1);
+  }
+
+})();
