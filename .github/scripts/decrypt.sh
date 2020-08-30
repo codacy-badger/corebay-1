8d8a69f65ec6a041bb6a36c1ebca610d7c8d651c
Mehavoid <tontole@riseup.net>
2020-08-30 18:55:13 +0300

    Move `config.js.gpg` to `.github/config`
    PR-URL: https://github.com/Mehavoid/corebay/pull/36

--- .github/scripts/decrypt.sh
+++ .github/scripts/decrypt.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
---output ./test/files/config.js ./test/files/config.js.gpg
+--output ./test/files/config.js ./.github/config/config.js.gpg
